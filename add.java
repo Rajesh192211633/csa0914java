8fc7c9ae87dd69146528a7751687197956cb1a86
Rajesh192211633 <123445768+Rajesh192211633@users.noreply.github.com>
2024-08-21 13:38:55 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add{
+    public static void main(String[] args){
+        int a=2;
+        int b=4;
+        int c;
+
+        c=a+b;
+        system.out.println(c);
+    }
+}
---
+++ reverse string.java
@@ -0,0 +1,8 @@
+public class r {
+    public static void main(String[] args) {
+        String n = "123";  
+        String n1 = new StringBuilder(n).reverse().toString();  
+
+        System.out.println(n1);  
+    }
+}
